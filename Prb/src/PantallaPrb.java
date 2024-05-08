fcabb3ab1f137e425b7951fa4c32db6b07bb6789
Macaco <160612719+avidmat0703@users.noreply.github.com>
2024-05-08 12:12:51 +0200

    Add files via upload

---
+++ Prb/src/PantallaPrb.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class PantallaPrb extends JFrame {
+    public PantallaPrb() {
+        setTitle("Tabla de Elementos de Ropa");
+
+        String[][] datos = {
+                {"Camiseta", "10", "S"},
+                {"Pantalones", "20", "M"},
+                {"Sudaderas", "15", "L"},
+                {"Calcetines", "5", "Única"},
+                {"Zapatos", "25", "42"},
+                {"Cinturones", "8", "M"},
+                {"Bañadores", "12", "L"}
+        };
+
+        String[] columnas = {"Prenda", "Stock", "Talla"};
+
+        JTable tabla = new JTable(datos, columnas);
+
+        JScrollPane scrollPane = new JScrollPane(tabla);
+
+        scrollPane.setPreferredSize(new Dimension(400, 200));
+
+        add(scrollPane, BorderLayout.CENTER);
+
+        setSize(500, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        PantallaPrb formulario = new PantallaPrb();
+        formulario.setVisible(true);
+    }
+}
