66568f5be1b74b8973662aa58d53322cb641804f
alejadrovidalmata <avidmat0703@g.educaand.es>
2024-05-08 12:18:31 +0200

    Commit 08/05/2024

---
+++ SQL/Tienda_de_ropa.sql
@@ -0,0 +1,23 @@
+Drop database if exists Tienda_de_ropa;
+Create database Tienda_de_ropa character set utf8mb4;
+Use Tienda_de_ropa; 
+
+CREATE table producto (
+	id INT AUTO_INCREMENT primary Key,
+	tipo_producto varchar(20),
+    stock int,
+    talla varchar(10),
+    color varchar(10),
+    marca varchar(20)
+);
+
+CREATE TABLE empleado (
+	DNI varchar(9) primary key,
+    nombre varchar(20),
+    apellido varchar(20),
+    apellido2 varchar(20),
+    email varchar(50),
+    telefono int,
+    puesto varchar(20)
+);
+
