df7396877e52403fe5cbf6ec9f3fd3e87d4886ae
Jose Cuevas <jcuemar0703@g.educaand.es>
2024-05-09 11:13:20 +0200

    Commit Jose

---
+++ ARKA/src/BACK/Venta.java
@@ -0,0 +1,47 @@
+package BACK;
+
+public class Venta {
+    private final String id = "id";
+    private int id_producto;
+    private int unidades;
+    private final String fecha = "now()";
+
+    public int getId_producto() {
+        return id_producto;
+    }
+
+    public void setId_producto(int id_producto) {
+        this.id_producto = id_producto;
+    }
+
+    public int getUnidades() {
+        return unidades;
+    }
+
+    public void setUnidades(int unidades) {
+        this.unidades = unidades;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public Venta(int id_producto, int unidades) {
+        this.id_producto = id_producto;
+        this.unidades = unidades;
+    }
+
+    @Override
+    public String toString() {
+        return "Venta{" +
+                "id='" + id + '\'' +
+                ", id_producto=" + id_producto +
+                ", unidades=" + unidades +
+                ", fecha='" + fecha + '\'' +
+                '}';
+    }
+}
