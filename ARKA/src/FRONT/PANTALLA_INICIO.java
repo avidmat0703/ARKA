5ea7df5d83a4181c8edb83ee9a0b2e5bc05ca5d1
alejadrovidalmata <avidmat0703@g.educaand.es>
2024-05-09 10:06:26 +0200

    Commit Alex

--- ARKA/src/FRONT/PANTALLA_INICIO.java
+++ ARKA/src/FRONT/PANTALLA_INICIO.java
@@ -9,21 +9,30 @@ public class PANTALLA_INICIO extends JFrame {
     public PANTALLA_INICIO() {
         // Configuración de la ventana principal
         setTitle("Pantalla Principal");
-        setSize(400, 270);
+        setSize(400, 310);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
 
-        JButton btnAltaBaja = new JButton("Alta y Baja de productos");
-        btnAltaBaja.setBounds(50, 20, 300, 30);
-        btnAltaBaja.addActionListener(new ActionListener() {
+        JButton btnAlta = new JButton("Alta de productos");
+        btnAlta.setBounds(50, 20, 300, 30);
+        btnAlta.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                new AltaBajaProductosFrame().setVisible(true);
+                new AltaProductosFrame().setVisible(true);
             }
         });
-        add(btnAltaBaja);
+        add(btnAlta);
+
+        JButton btnBaja = new JButton("Baja de productos");
+        btnBaja.setBounds(50, 60, 300, 30);
+        btnBaja.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                new BajaProductosFrame().setVisible(true);
+            }
+        });
+        add(btnBaja);
 
         JButton btnModificar = new JButton("Modificar producto");
-        btnModificar.setBounds(50, 60, 300, 30);
+        btnModificar.setBounds(50, 100, 300, 30);
         btnModificar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 new ModificarProductoFrame().setVisible(true);
@@ -32,7 +41,7 @@ public class PANTALLA_INICIO extends JFrame {
         add(btnModificar);
 
         JButton btnConsultarInventario = new JButton("Consultar inventario");
-        btnConsultarInventario.setBounds(50, 100, 300, 30);
+        btnConsultarInventario.setBounds(50, 140, 300, 30);
         btnConsultarInventario.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 new ConsultarInventarioFrame().setVisible(true);
@@ -41,7 +50,7 @@ public class PANTALLA_INICIO extends JFrame {
         add(btnConsultarInventario);
 
         JButton btnRealizarVenta = new JButton("Realizar Venta");
-        btnRealizarVenta.setBounds(50, 140, 300, 30);
+        btnRealizarVenta.setBounds(50, 180, 300, 30);
         btnRealizarVenta.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 new RealizarVentaFrame().setVisible(true);
@@ -50,7 +59,7 @@ public class PANTALLA_INICIO extends JFrame {
         add(btnRealizarVenta);
 
         JButton btnGestionarEmpleados = new JButton("Gestionar empleados");
-        btnGestionarEmpleados.setBounds(50, 180, 300, 30);
+        btnGestionarEmpleados.setBounds(50, 220, 300, 30);
         btnGestionarEmpleados.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 new GestionarEmpleadosFrame().setVisible(true);
@@ -62,12 +71,25 @@ public class PANTALLA_INICIO extends JFrame {
     public static void main(String[] args) {
         PANTALLA_INICIO pantallaInicio = new PANTALLA_INICIO();
         pantallaInicio.setVisible(true);
+        try{
+            UIManager.setLookAndFeel(com.);
+        } catch(Exception e){
+            System.out.println(e);
+        }
     }
 }
 
-class AltaBajaProductosFrame extends JFrame {
-    public AltaBajaProductosFrame() {
-        setTitle("Alta y Baja de Productos");
+class AltaProductosFrame extends JFrame {
+    public AltaProductosFrame() {
+        setTitle("Alta de Productos");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
+}
+
+class BajaProductosFrame extends JFrame {
+    public BajaProductosFrame() {
+        setTitle("Baja de Productos");
         setSize(300, 200);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     }
