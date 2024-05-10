2c03acbd4f985154b5e26d8f5a04045813a11de6
Alex <avidmat0703@g.educaand.es>
2024-05-10 17:15:27 +0200

    Commit Alex

--- ARKA/src/FRONT/PANTALLA_INICIO.java
+++ ARKA/src/FRONT/PANTALLA_INICIO.java
@@ -1,7 +1,6 @@
 package FRONT;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -139,10 +138,49 @@ class AltaProductosFrame extends JFrame {
 }
 
 class BajaProductosFrame extends JFrame {
+    private JTextField idField;
+
     public BajaProductosFrame() {
-        setTitle("Baja de producto");
-        setSize(300, 200);
+        setTitle("Baja de Producto");
+        setSize(400, 200);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+
+        addFieldWithMargin("ID del producto:", idField = new JTextField(20));
+
+        JButton listarButton = new JButton("Listar todos los productos");
+        listarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new ConsultarInventarioFrame().setVisible(true);
+            }
+        });
+
+        JButton bajaButton = new JButton("Dar de baja este producto");
+        bajaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+        buttonPanel.add(listarButton);
+        buttonPanel.add(Box.createVerticalStrut(10));
+        buttonPanel.add(bajaButton);
+        buttonPanel.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+        add(buttonPanel);
+    }
+
+    private void addFieldWithMargin(String label, JTextField field) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+        panel.add(new JLabel(label));
+        panel.add(Box.createHorizontalStrut(10));
+        panel.add(field);
+        add(panel);
     }
 }
 
