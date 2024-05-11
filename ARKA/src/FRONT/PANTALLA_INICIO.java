ec060db3c7004438ec5c135d096c61f0404fd63e
Alex <avidmat0703@g.educaand.es>
2024-05-11 19:27:08 +0200

    Commit Alex

--- ARKA/src/FRONT/PANTALLA_INICIO.java
+++ ARKA/src/FRONT/PANTALLA_INICIO.java
@@ -220,21 +220,30 @@ class ModificarProductoFrame extends JFrame {
     private JTextField marcaField;
     private JTextField precioField;
 
+    private JCheckBox chkCodigo;
+    private JCheckBox chkNombre;
+    private JCheckBox chkStock;
+    private JCheckBox chkTalla;
+    private JCheckBox chkDescripcion;
+    private JCheckBox chkColor;
+    private JCheckBox chkMarca;
+    private JCheckBox chkPrecio;
+
     public ModificarProductoFrame() {
         setTitle("Modificar Producto");
-        setSize(450, 500);
+        setSize(450, 520);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
 
         addFieldWithMargin("ID del Producto:", idField = new JTextField(20));
-        addFieldWithMargin("Código en tienda:", codigoField = new JTextField(20));
-        addFieldWithMargin("Nombre:", nombreField = new JTextField(20));
-        addFieldWithMargin("Stock:", stockField = new JTextField(20));
-        addFieldWithMargin("Talla:", tallaField = new JTextField(20));
-        addFieldWithMargin("Descripción:", descripcionField = new JTextField(20));
-        addFieldWithMargin("Color:", colorField = new JTextField(20));
-        addFieldWithMargin("Marca:", marcaField = new JTextField(20));
-        addFieldWithMargin("Precio:", precioField = new JTextField(20));
+        addFieldWithMargin("Código en tienda:", codigoField = new JTextField(20), chkCodigo = new JCheckBox());
+        addFieldWithMargin("Nombre:", nombreField = new JTextField(20), chkNombre = new JCheckBox());
+        addFieldWithMargin("Stock:", stockField = new JTextField(20), chkStock = new JCheckBox());
+        addFieldWithMargin("Talla:", tallaField = new JTextField(20), chkTalla = new JCheckBox());
+        addFieldWithMargin("Descripción:", descripcionField = new JTextField(20), chkDescripcion = new JCheckBox());
+        addFieldWithMargin("Color:", colorField = new JTextField(20), chkColor = new JCheckBox());
+        addFieldWithMargin("Marca:", marcaField = new JTextField(20), chkMarca = new JCheckBox());
+        addFieldWithMargin("Precio:", precioField = new JTextField(20), chkPrecio = new JCheckBox());
 
         JButton listarButton = new JButton("Listar todos los productos");
         listarButton.addActionListener(new ActionListener() {
@@ -248,7 +257,32 @@ class ModificarProductoFrame extends JFrame {
         modificarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(ModificarProductoFrame.this, "Producto modificado correctamente");
+                StringBuilder cambios = new StringBuilder("Cambios realizados:\n");
+                if (chkCodigo.isSelected()) {
+                    cambios.append(" - Código en tienda modificado: ").append(codigoField.getText()).append("\n");
+                }
+                if (chkNombre.isSelected()) {
+                    cambios.append(" - Nombre modificado: ").append(nombreField.getText()).append("\n");
+                }
+                if (chkStock.isSelected()) {
+                    cambios.append(" - Stock modificado: ").append(stockField.getText()).append("\n");
+                }
+                if (chkTalla.isSelected()) {
+                    cambios.append(" - Talla modificada: ").append(tallaField.getText()).append("\n");
+                }
+                if (chkDescripcion.isSelected()) {
+                    cambios.append(" - Descripción modificada: ").append(descripcionField.getText()).append("\n");
+                }
+                if (chkColor.isSelected()) {
+                    cambios.append(" - Color modificado: ").append(colorField.getText()).append("\n");
+                }
+                if (chkMarca.isSelected()) {
+                    cambios.append(" - Marca modificada: ").append(marcaField.getText()).append("\n");
+                }
+                if (chkPrecio.isSelected()) {
+                    cambios.append(" - Precio modificado: ").append(precioField.getText()).append("\n");
+                }
+                JOptionPane.showMessageDialog(ModificarProductoFrame.this, cambios.toString());
                 dispose();
             }
         });
@@ -260,6 +294,18 @@ class ModificarProductoFrame extends JFrame {
         buttonPanel.add(modificarButton);
         buttonPanel.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
         add(buttonPanel);
+    }
+
+    private void addFieldWithMargin(String label, JTextField field, JCheckBox checkBox) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+        panel.add(checkBox);
+        panel.add(Box.createHorizontalStrut(10));
+        panel.add(new JLabel(label));
+        panel.add(Box.createHorizontalStrut(10));
+        panel.add(field);
+        add(panel);
     }
 
     private void addFieldWithMargin(String label, JTextField field) {
