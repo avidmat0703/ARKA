86289ff361120698eaa433a85b5cd61142d4b22d
davidvalenzuelafarias <dvalfar1305@g.educaand.es>
2024-05-13 10:46:28 +0200

    si

--- ARKA/src/FRONT/PANTALLA_INICIO.java
+++ ARKA/src/FRONT/PANTALLA_INICIO.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class PANTALLA_INICIO extends JFrame {
+
+    private String info = "";
     public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel("com.jtattoo.plaf.bernstein.BernsteinLookAndFeel");
@@ -447,54 +449,6 @@ class AltaEmpleadoFrame extends JFrame {
         JPanel buttonPanel = new JPanel();
         buttonPanel.add(altaButton);
         buttonPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
-        add(buttonPanel);
-    }
-
-    private void addFieldWithMargin(String label, JTextField field) {
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
-        panel.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
-        panel.add(new JLabel(label));
-        panel.add(Box.createHorizontalStrut(10));
-        panel.add(field);
-        add(panel);
-    }
-}
-
-class BajaEmpleadoFrame extends JFrame {
-    private JTextField DNIField;
-
-    public BajaEmpleadoFrame() {
-        setTitle("Baja de emplado");
-        setSize(350, 170);
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
-
-        addFieldWithMargin("DNI del empleado:", DNIField = new JTextField(20));
-
-        JButton listarButton = new JButton("Listar todos los empleados");
-        listarButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new ConsultarEmpleadoFrame().setVisible(true);
-            }
-        });
-
-        JButton bajaButton = new JButton("   Eliminar este empleado   ");
-        bajaButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(BajaEmpleadoFrame.this, "Empleado eliminado correctamente");
-                dispose();
-            }
-        });
-
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
-        buttonPanel.add(listarButton);
-        buttonPanel.add(Box.createVerticalStrut(15));
-        buttonPanel.add(bajaButton);
-        buttonPanel.setBorder(BorderFactory.createEmptyBorder(15, 20, 15, 20));
         add(buttonPanel);
     }
 
